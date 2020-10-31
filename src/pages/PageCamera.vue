71b6ab1dfb6f0ba4700882769761962e80a588c2
Adir Noyman <45500652+AdirNoyman@users.noreply.github.com>
2020-10-31 18:18:38 +0200

    Add files via upload

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -1,10 +1,11 @@
 <template>
   <q-page class="constrain-camera q-pa-md">
     <div class="camera-frame q-pa-md">
-      <img class="full-width" src='https://cdn.quasar.dev/img/mountains.jpg'>
+      <video ref="video" class="full-width" autoplay v-show="!imageCaptured"/>
+      <canvas class="full-width" ref="canvas" height="240" v-show="imageCaptured"/>
     </div>
     <div class="text-center q-pa-md">
-      <q-btn round color="grey-10" icon="fas fa-camera" size="1.2em" />
+      <q-btn @click="captureImage" round color="grey-10" icon="fas fa-camera" size="1.2em" />
       <div class="row justify-center q-ma-md">
         <q-input v-model="post.caption" label="Caption" class="col-10 col-sm-8"/>        
       </div>
@@ -26,6 +27,7 @@
 
 <script>
 import { uid } from 'quasar'
+require('md-gum-polyfill')
 export default {
   name: 'PageCamera',
   data() {
@@ -39,17 +41,50 @@ export default {
         location: "",
         photo: null,
         date: Date.now()
-      }
+      },
+      imageCaptured: false
 
     }
 
-  }
+  },
+ methods: {
+
+   initCamera() {
+
+     navigator.mediaDevices.getUserMedia({
+
+       video: true
+
+     }).then(stream => {
+
+       this.$refs.video.srcObject = stream
+
+     })
+
+   },
+   captureImage() {
+
+    let video = this.$refs.video
+    let canvas = this.$refs.canvas
+    canvas.width = video.getBoundingClientRect().width
+    canvas.height = video.getBoundingClientRect().height
+    let context = canvas.getContext('2d')
+    context.drawImage(video,0,0,canvas.width,canvas.height)
+    this.imageCaptured = true
+
+   }
+   },
+   // Vue function Hook 
+   mounted() {
+
+     this.initCamera();
+
+   } 
 }
 </script>
 
 <style lang="sass">
-.camera-frame img
-  border: 1px solid $grey-7
+.camera-frame
+  border: 2px solid $grey-7
   border-radius: 7px
-
 </style>
