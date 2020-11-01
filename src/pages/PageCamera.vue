26e3d54467931c1eb2540c9257afccd72edec116
Adir Noyman <45500652+AdirNoyman@users.noreply.github.com>
2020-11-01 20:03:44 +0200

    Add files via upload

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -1,11 +1,35 @@
 <template>
   <q-page class="constrain-camera q-pa-md">
     <div class="camera-frame q-pa-md">
-      <video ref="video" class="full-width" autoplay v-show="!imageCaptured"/>
+      <video
+       ref="video"
+       class="full-width"
+       autoplay
+       v-show="!imageCaptured"
+       playsinline
+       />
       <canvas class="full-width" ref="canvas" height="240" v-show="imageCaptured"/>
     </div>
     <div class="text-center q-pa-md">
-      <q-btn @click="captureImage" round color="grey-10" icon="fas fa-camera" size="1.2em" />
+      <q-btn
+       v-if="hasCameraSupport"
+       @click="captureImage"
+       round color="grey-10"
+       icon="fas fa-camera"
+       size="1.2em"
+        />
+      <q-file
+       v-else
+       @input="captureImageFallBack"
+       outlined
+       v-model="imageUpLoad"
+       label="Choose an image"
+       accept="image/*"
+       >
+        <template v-slot:prepend>
+          <q-icon name="fas fa-paperclip" />
+        </template>
+      </q-file>
       <div class="row justify-center q-ma-md">
         <q-input v-model="post.caption" label="Caption" class="col-10 col-sm-8"/>        
       </div>
@@ -42,7 +66,9 @@ export default {
         photo: null,
         date: Date.now()
       },
-      imageCaptured: false
+      imageCaptured: false,
+      imageUpLoad: [],
+      hasCameraSupport: true
 
     }
 
@@ -59,6 +85,10 @@ export default {
 
        this.$refs.video.srcObject = stream
 
+     }).catch(error => {
+
+       this.hasCameraSupport = false
+
      })
 
    },
@@ -71,15 +101,77 @@ export default {
     let context = canvas.getContext('2d')
     context.drawImage(video,0,0,canvas.width,canvas.height)
     this.imageCaptured = true
-
-   }
+    this.post.photo = this.dataURItoBlob(canvas.toDataURL());
+    this.disableCamera();
    },
-   // Vue function Hook 
+   captureImageFallBack(file) {
+     this.post.photo = file;
+     let canvas = this.$refs.canvas
+     let context = canvas.getContext('2d')
+     let reader = new FileReader();
+    reader.onload = (event) => {
+        let img = new Image();
+        img.onload = () => {
+            canvas.width = img.width;
+            canvas.height = img.height;
+            context.drawImage(img,0,0);
+            this.imageCaptured = true
+        }
+        img.src = event.target.result;
+    }
+    reader.readAsDataURL(file); 
+
+   },
+   disableCamera() {
+
+     this.$refs.video.srcObject.getVideoTracks().forEach(track => {
+
+       track.stop();
+
+     });
+
+   },
+   dataURItoBlob(dataURI) {
+  // convert base64 to raw binary data held in a string
+  // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+  let byteString = atob(dataURI.split(',')[1]);
+
+  // separate out the mime component
+  let mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+
+  // write the bytes of the string to an ArrayBuffer
+  let ab = new ArrayBuffer(byteString.length);
+
+  // create a view into the buffer
+  let ia = new Uint8Array(ab);
+
+  // set the bytes of the buffer to the correct values
+  for (let i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+  }
+
+  // write the ArrayBuffer to a blob, and you're done
+  let blob = new Blob([ab], {type: mimeString});
+  return blob;
+
+}
+   },
+   // Vue lifesycle Hooks /////////////////////////////////////////////// 
    mounted() {
 
      this.initCamera();
 
-   } 
+   },
+   beforeDestroy() {
+
+     if (this.hasCameraSupport) {
+
+       this.disableCamera();
+
+     }
+     
+
+   }
 }
 </script>
 
